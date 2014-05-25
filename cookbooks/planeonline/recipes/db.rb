9fbe51c8fc3c73725471d84eae4ee99e27075e57
Ali Bahman <ABN@Ali-Bahmans-MacBook-Pro.local>
2014-05-25 17:29:29 +0100

    Adding dev and test databases and their users

--- cookbooks/planeonline/recipes/db.rb
+++ cookbooks/planeonline/recipes/db.rb
@@ -13,6 +13,12 @@ bash 'setup_phalconphp_db' do
   service mysqld restart
   mysql -u root -pabn -e "CREATE SCHEMA planeonline DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
   mysql -u root -pabn -e "GRANT ALL ON planeonline.* TO 'planeuser'@'localhost' IDENTIFIED BY 'planepass'";
+
+  mysql -u root -pabn -e "CREATE SCHEMA planeonline_dev DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+  mysql -u root -pabn -e "GRANT ALL ON planeonline_dev.* TO 'planeuser_dev'@'localhost' IDENTIFIED BY 'planepass'";
+
+  mysql -u root -pabn -e "CREATE SCHEMA planeonline_test DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+  mysql -u root -pabn -e "GRANT ALL ON planeonline_test.* TO 'planeuser_test'@'localhost' IDENTIFIED BY 'planepass'";
   EOL
 end
 
